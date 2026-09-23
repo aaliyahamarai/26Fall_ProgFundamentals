94dd8ff4860eae6000084198a0539217f4ebb330
aaliyahamarai <awilson88@twu.edu>
2026-09-23 16:25:50 -0500

    Completed assignment 4

---
+++ Assignments/Assignment_4/q1.py
@@ -0,0 +1,5 @@
+name = "Aaliyah"
+age = 20
+height = 1.55
+
+print(f"{name} is {age} years old and {height} m tall.")
---
+++ Assignments/Assignment_4/q3.py
@@ -0,0 +1,9 @@
+mark1 = 85
+mark2 = 78
+mark3 = 92
+
+total = mark1 + mark2 + mark3
+average = total / 3
+
+print(f"Total: {total} out of 300")
+print(f"Average: {average:.2f}%")
